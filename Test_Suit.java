ecbfce1afd60dd0bcd6b60fe641fbc87775c598c
Kaustav14 <kaustavgamguly@gmail.com>
2023-08-16 12:19:04 +0530

    Eleventh Commit

--- Test_Suit.java
+++ Test_Suit.java
@@ -6,8 +6,8 @@ public class Test_Suit
     void testmoveforward()
     {
         int input[]={0,0,0};
-        Sample_Code sc=new Sample_Code(input,"W");
-        assertArrayEquals(new int[]{-1,0,0}, sc.moveforward());
+        Sample_Code sc=new Sample_Code(input,"Up");
+        assertArrayEquals(new int[]{0,0,1}, sc.moveforward());
 
     }
 
