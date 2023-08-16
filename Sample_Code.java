eab3542eeaf46851b96ca55f51c6f7ebfef9c787
Kaustav14 <kaustavgamguly@gmail.com>
2023-08-16 12:06:54 +0530

    Sixth Commit

--- Sample_Code.java
+++ Sample_Code.java
@@ -10,7 +10,10 @@ public class Sample_Code
     }
     public int[] moveforward()
     {
+        if(int_dir=="N")
         int_pos[1]++;
+        else if(int_dir=="S")
+        int_pos[1]--;
         return int_pos;
     }
 public static void main(String args[])
