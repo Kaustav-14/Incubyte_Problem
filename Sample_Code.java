717ba275d480b8fbcdb276d9aa17c9d086f677dc
Kaustav14 <kaustavgamguly@gmail.com>
2023-08-16 16:51:58 +0530

    Fortyth Commit

--- Sample_Code.java
+++ Sample_Code.java
@@ -1,3 +1,4 @@
+import java.util.Scanner; 
 public class Sample_Code
 {
     String actual_dir;
@@ -11,15 +12,15 @@ public class Sample_Code
     }
     public int[] moveforward()
     {
-        if(int_dir=="N")
+        if(int_dir.equals("N"))
         int_pos[1]++;
-        else if(int_dir=="S")
+        else if(int_dir.equals("S"))
         int_pos[1]--;
-        else if(int_dir=="E")
+        else if(int_dir.equals("E"))
         int_pos[0]++;
-        else if(int_dir=="W")
+        else if(int_dir.equals("W"))
         int_pos[0]--;
-        else if(int_dir=="U")
+        else if(int_dir.equals("U"))
         int_pos[2]++;
         else
         int_pos[2]--;
@@ -27,15 +28,15 @@ public class Sample_Code
     }
     public int[] movebackward()
     {
-        if(int_dir=="N")
+        if(int_dir.equals("N"))
         int_pos[1]--;
-        else if(int_dir=="S")
+        else if(int_dir.equals("S"))
         int_pos[1]++;
-        else if(int_dir=="E")
+        else if(int_dir.equals("E"))
         int_pos[0]--;
-        else if(int_dir=="W")
+        else if(int_dir.equals("W"))
         int_pos[0]++;
-        else if(int_dir=="U")
+        else if(int_dir.equals("U"))
         int_pos[2]--;
         else
         int_pos[2]++;
@@ -43,11 +44,11 @@ public class Sample_Code
     }
     public String turnright()
     {
-        if(int_dir=="N")
+        if(int_dir.equals("N"))
         int_dir="E";
-        else if(int_dir=="E")
+        else if(int_dir.equals("E"))
         int_dir="S";
-        else if(int_dir=="S")
+        else if(int_dir.equals("S"))
         int_dir="W";
         else
         int_dir="N";
@@ -55,11 +56,11 @@ public class Sample_Code
     }
     public String turnleft()
     {
-        if(int_dir=="W")
+        if(int_dir.equals("W"))
         int_dir="S";
-        else if(int_dir=="S")
+        else if(int_dir.equals("S"))
         int_dir="E";
-        else if(int_dir=="E")
+        else if(int_dir.equals("E"))
         int_dir="N";
         else
         int_dir="W";
@@ -83,18 +84,19 @@ public class Sample_Code
         {
             switch(commands[i])
             {
-                case 'f':moveforward();break;
-                case 'b':movebackward();break;
+                case 'f':int_pos=moveforward();
+                         break;
+                case 'b':int_pos=movebackward();break;
                 case 'l':if(int_dir=="U"||int_dir=="D")
                          int_dir=actual_dir;
-                         turnleft();
+                         int_dir=turnleft();
                          break;
                 case 'r':if(int_dir=="U"||int_dir=="D")
                          int_dir=actual_dir;
-                         turnright();
+                         int_dir=turnright();
                          break;
-                case 'u':upward();break;
-                default:downward();
+                case 'u':int_dir=upward();break;
+                default:int_dir=downward();
             }
         }
          return int_pos;
@@ -103,8 +105,25 @@ public class Sample_Code
     {
         return int_dir;
     }
-public static void main(String args[])
-{
-   
-}
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter initial direction");
+        String int_dir=s.nextLine();
+        int int_pos[]=new int[3];
+        System.out.println("Enter initial x, y and z co-ordinates");
+        int_pos[0]=s.nextInt();
+        int_pos[1]=s.nextInt();
+        int_pos[2]=s.nextInt();
+        Sample_Code obj=new Sample_Code(int_pos, int_dir);
+        System.out.println("Enter no.of commands");
+        int n=s.nextInt();
+        char commands[]=new char[n];
+        for(int i=0;i<n;i++)
+        commands[i]=s.next().charAt(0);
+        int result_pos[]=obj.sample_position(commands);
+        String result_dir=obj.sample_direction();
+        System.out.println("Final Position: {"+result_pos[0]+","+result_pos[1]+","+result_pos[2]+"}");
+        System.out.println("Final Direction: "+result_dir);
+    }
 }
