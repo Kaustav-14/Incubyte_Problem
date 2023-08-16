69830b868ae798bf3cda4e615e908cead1f58cd7
Kaustav14 <kaustavgamguly@gmail.com>
2023-08-16 12:58:12 +0530

    Twenty Sixth Commit

--- Test_Suit.java
+++ Test_Suit.java
@@ -3,25 +3,24 @@ import org.junit.jupiter.api.Test;
 public class Test_Suit
 {
     int input[]={0,0,0};
-    String input_dir="N";
     @Test
     void testmoveforward()
     {
-        Sample_Code sc=new Sample_Code(input,input_dir);
+        Sample_Code sc=new Sample_Code(input,"N");
         assertArrayEquals(new int[]{0,1,0}, sc.moveforward());
 
     }
     @Test
     void testmovebackward()
     {
-        Sample_Code sc=new Sample_Code(input, input_dir);
+        Sample_Code sc=new Sample_Code(input, "N");
         assertArrayEquals(new int[]{0,-1,0}, sc.movebackward()); 
     }
     @Test
     void testturnright()
     {
-        Sample_Code sc=new Sample_Code(input, input_dir);
-        assertEquals("E", sc.turnright());
+        Sample_Code sc=new Sample_Code(input, "E");
+        assertEquals("S", sc.turnright());
          
     }
 
