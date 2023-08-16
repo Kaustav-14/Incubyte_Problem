392fd1bdfb1962c9f197968fb4e21e78f5340cd5
Kaustav14 <kaustavgamguly@gmail.com>
2023-08-16 11:42:01 +0530

    Third Commit

--- Test_Suit.java
+++ Test_Suit.java
@@ -6,6 +6,9 @@ public class Test_Suit
     void testmoveforward()
     {
         Sample_Code sc=new Sample_Code();
+        int init_pos[]={0,0,0};
+        String init_dir="N";
+        assertArrayEquals(new int[]{0,1,0}, sc.moveforward(init_pos,init_dir));
     }
 
 }
