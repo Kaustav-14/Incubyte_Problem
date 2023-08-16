fdb887cbf1f0bebdd3ee14e3cc5c9b9a7f7b78f7
Kaustav14 <kaustavgamguly@gmail.com>
2023-08-16 11:59:15 +0530

    Fourth Commit

--- Sample_Code.java
+++ Sample_Code.java
@@ -1,7 +1,20 @@
 public class Sample_Code
 {
+    int int_pos[]=new int[3];
+    String int_dir;
+    public Sample_Code(int input_pos[], String input_dir)
+    {
+        for(int i=0;i<3;i++)
+        int_pos[i]=input_pos[i];
+        int_dir=input_dir;
+    }
+    public int[] moveforward()
+    {
+        int_pos[1]++;
+        return int_pos;
+    }
 public static void main(String args[])
 {
-    
+   
 }
 }
--- Test_Suit.java
+++ Test_Suit.java
@@ -5,10 +5,9 @@ public class Test_Suit
     @Test
     void testmoveforward()
     {
-        Sample_Code sc=new Sample_Code();
-        int init_pos[]={0,0,0};
-        String init_dir="N";
-        assertArrayEquals(new int[]{0,1,0}, sc.moveforward(init_pos,init_dir));
+        int input[]={0,0,0};
+        Sample_Code sc=new Sample_Code(input,"N");
+        assertArrayEquals(new int[]{0,1,0}, sc.moveforward());
     }
 
 }
