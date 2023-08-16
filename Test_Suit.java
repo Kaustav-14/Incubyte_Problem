2dfa761b2f519f52df870766510485736cc7118f
Kaustav14 <kaustavgamguly@gmail.com>
2023-08-16 12:34:06 +0530

    Fifteenth Commit

--- Test_Suit.java
+++ Test_Suit.java
@@ -13,8 +13,8 @@ public class Test_Suit
     @Test
     void testmovebackward()
     {
-        Sample_Code sc=new Sample_Code(input, "N");
-        assertArrayEquals(new int[]{0,-1,0}, sc.movebackward()); 
+        Sample_Code sc=new Sample_Code(input, "S");
+        assertArrayEquals(new int[]{0,1,0}, sc.movebackward()); 
     }
 
 }
